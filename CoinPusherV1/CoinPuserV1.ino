f5ba41f78937c8cdea83275723d996c8fc9e7386
Justin Trantham <takescake@outlook.com>
2018-09-04 23:23:52 -0500

    comments
    Added comments

--- CoinPusherV1/CoinPuserV1.ino
+++ CoinPusherV1/CoinPuserV1.ino
@@ -1,4 +1,4 @@
-#include <Servo.h> 
+#include <Servo.h>
 
 Servo servo;
 const int ServoA = 2;   //"Pusher" Servo D4
@@ -6,26 +6,26 @@ const int ButtonA = 4; //"Button Green" D2
 
 void setup() {
   Serial.begin(9600);
-   pinMode(ButtonA, INPUT_PULLUP); 
-   
-  servo.attach(ServoA); 
+   pinMode(ButtonA, INPUT_PULLUP);
+
+  servo.attach(ServoA);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-       if ((digitalRead(ButtonA) == 0)){
-                             
-       }
-       else{
-        rotate();
-       }
-  Serial.println(digitalRead(ButtonA));
+   //digitalRead(ButtonA) == 0; Means pressed
+   if ((digitalRead(ButtonA) == 0)){
+
+   }
+   //Not Pressed
+   else{
+    rotate();
+   }
 }
-void rotate(){ 
+void rotate(){
      digitalWrite(ServoA,HIGH);
      delayMicroseconds(1400); //1300 is fast, 1499 is very slow
      digitalWrite(ServoA,LOW);
-     delay(20); // 20.0ms 
+     delay(20); // 20.0ms
      //delay(50);// enable this line to slow servo rotation
 }
